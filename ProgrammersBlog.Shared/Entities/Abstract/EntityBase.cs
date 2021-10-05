d62ce0b77e52579d5528b008c0ab304d8355bb4f
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-05 15:59:33 +0300

    base entitties added in shared

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+   public abstract class EntityBase//  ** our class is a abstract class , here so abstract uses **
+    {
+        public virtual int Id { get; set; } // ** and the objects can be override , virtual because of this **
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        public virtual bool IsDeleted { get; set; } = false;
+
+        public virtual bool IsActive{ get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+
+
+    }
+}
---
+++ ProgrammersBlog.Shared/Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    interface IEntity// it is a signature for repository pattern
+    {
+    }
+}
