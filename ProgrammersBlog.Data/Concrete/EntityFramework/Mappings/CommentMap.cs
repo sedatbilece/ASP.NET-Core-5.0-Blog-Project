71e28643f79e3ea11c94f09e82e5e59e0e358003
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-12 12:35:13 +0300

    category mapping

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/CommentMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class CommentMap : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");// table name
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Text).IsRequired();
+            builder.Property(c => c.Text).HasMaxLength(2000);
+
+
+
+
+        }
+    }
+}
