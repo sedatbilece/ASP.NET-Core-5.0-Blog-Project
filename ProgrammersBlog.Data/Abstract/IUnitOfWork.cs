6690818fd581ab85130b848dc21824f068052243
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-14 19:40:59 +0300

    UnitOfWork structure added

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+   public  interface IUnitOfWork :IAsyncDisposable
+    {
+
+        IArticleRepository Articles { get; } // _unitofwork.Articles
+        ICategoryRepository Categories { get; } //_unitofwork.Categories.AddAsync(....);
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository User { get; }
+
+        Task<int> SaveAsync();
+
+
+
+
+    }
+}
