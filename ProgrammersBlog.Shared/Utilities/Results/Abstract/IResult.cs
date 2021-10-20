5a0ee531bcd3848dcd5c0feab51a735d7c0767ed
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-20 11:53:36 +0300

    Result and IResult added to shared layer

---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        // this is in the complex type folder type is a enum 
+        public ResultStatus ResultStatus { get;  }//ResultStatus.success // ResultStatus.error like that 
+        public String Message { get;  }
+        public Exception Exception { get;  }
+    }
+}
