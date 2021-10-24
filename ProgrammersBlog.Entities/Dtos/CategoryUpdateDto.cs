662bac284fbcde6ab7503a173a04a8887095b818
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-24 17:34:31 +0300

    category update dto added

--- ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,36 @@ namespace ProgrammersBlog.Entities.Dtos
 {
    public class CategoryUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden fazla olamaz")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz")]
+        public string Name { get; set; }
+
+
+
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olamaz")]
+        [MinLength(10, ErrorMessage = "{0} {1} karakterden az olamaz")]
+        public string Description { get; set; }
+
+
+        [DisplayName(" Not alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olamaz")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olamaz")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif mi ?")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        public bool IsActive { get; set; }
+
+
+        [DisplayName("Silindi mi ?")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        public bool IsDeleted { get; set; }
+
     }
 }
