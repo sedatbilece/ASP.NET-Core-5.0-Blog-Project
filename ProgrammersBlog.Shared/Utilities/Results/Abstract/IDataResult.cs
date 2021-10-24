fc98fdad908e706509a5fa5742af0f36de2d4a54
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-24 16:40:41 +0300

    DataResult added

---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+   public  interface IDataResult<out T> : IResult //out : genus information will come from outside
+    {
+
+        public T Data { get;  }//new DataResult<Category>(ResultStatus.success , category ) ;
+                               //new DataResult< IList<Category> >(ResultStatus.success , categorylist ) ;
+    }
+}
