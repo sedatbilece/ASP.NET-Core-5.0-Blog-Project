b94c9f9c5c973d80803c797ce4dc791af2738fd8
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-10-24 17:19:44 +0300

    categoryService and Dtos added

---
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,26 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+   public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+
+        Task< IDataResult< IList<Category> > > GetAll();
+
+        Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);//dto(data transfer object )
+
+        Task<IResult> Update(CategoryAddDto categoryUpdateDto, string modifiedByName);
+
+        Task<IResult> Delete(int categoryId);
+
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
